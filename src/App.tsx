7b30138ff15414fad8ba87ad78aad9180d6bd72f
caralovell <168685711+caralovell@users.noreply.github.com>
2026-06-09 08:41:43 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/situmCatalogo">
+      <BrowserRouter basename="/catrepsol">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
