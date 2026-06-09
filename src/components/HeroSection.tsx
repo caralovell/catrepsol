e70ae46cb0bbfdaf5237a9fc42f45a60b0d49a22
caralovell <168685711+caralovell@users.noreply.github.com>
2026-06-09 12:49:05 +0200

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import LogopostLogo from "./LogopostLogo";
-import situm from "@/assets/situm.png";
-import hposh from "@/assets/hposhh.png";
+import situm from "@/assets/logo-repsol.png";
+import hposh from "@/assets/maiin.png";
 
 const fadeUp = (delay: number) => ({
   initial: { opacity: 0, y: 16 },
